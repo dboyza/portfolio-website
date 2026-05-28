ffd4af9320b1387cf4af7410be86213fba187aae
Dylan Boyza <dboyza19@gmail.com>
2026-05-27 22:46:16 -0400

    Update minor cryptography focus

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -198,7 +198,7 @@ export const degrees: Degree[] = [
     timeframe: '2019-2023',
     description:
       'Use this block for another completed program, in-progress degree, certificate program, or relevant coursework.',
-    focusAreas: ['Networking', 'Security fundamentals', 'Linux'],
+    focusAreas: ['Networking', 'Cryptography', 'Linux'],
   },
 ];
 
